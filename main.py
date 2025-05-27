abdb659ee3fb0d1b07c32b010ff505603f58f558
Andrik1591 <kinglouie1591@gmx.de>
2025-05-27 14:46:33 +0200

    main.py

--- main.py
+++ main.py
@@ -48,7 +48,11 @@ def send_telegram_message(text):
 def is_product_available(url):
     try:
         headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+            "Accept-Language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Referer": "https://www.google.com/",
+            "Connection": "keep-alive"
         }
         response = requests.get(url, headers=headers)
         response.raise_for_status()
