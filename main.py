f6d300a956e6781846460cbcf3ac4ca42480698d
Andrik1591 <kinglouie1591@gmx.de>
2025-05-28 15:25:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -56,13 +56,14 @@ def is_product_available(url):
             print("[INFO] Verwende Selenium für Smyths")
 
             chrome_options = Options()
+            chrome_options.binary_location = "/usr/bin/chromium-browser"  # Pfad zu Chromium in Render
+
             chrome_options.add_argument("--headless=new")
             chrome_options.add_argument("--disable-gpu")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--window-size=1920,1080")
 
-            # webdriver-manager Service automatisch ChromeDriver managen lassen
             service = Service(ChromeDriverManager().install())
             driver = webdriver.Chrome(service=service, options=chrome_options)
 
