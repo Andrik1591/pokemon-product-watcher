b3e5ffd208bd3705bb1cf82981f5623277f87c2e
Andrik1591 <kinglouie1591@gmx.de>
2025-05-28 15:19:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,10 @@ def is_product_available(url):
             chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--window-size=1920,1080")
 
-            driver = webdriver.Chrome(executable_path="./chromedriver_render", options=chrome_options)
+            # webdriver-manager Service automatisch ChromeDriver managen lassen
+            service = Service(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=chrome_options)
+
             driver.get(url)
             time.sleep(4)
 
