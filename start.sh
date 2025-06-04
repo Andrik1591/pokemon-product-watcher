38d6631144e133c4d728caf0058a09f7a030b261
Andrik1591 <kinglouie1591@gmx.de>
2025-06-04 07:40:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
 echo "🚀 Starte Produktchecker..."
+
+# Versionsinfos loggen
+echo "🧪 Chrome-Version:"
+google-chrome --version || which google-chrome
+
+echo "🧪 ChromeDriver-Version:"
+chromedriver --version || which chromedriver
+
+# Starte das Python-Skript
 python main.py
